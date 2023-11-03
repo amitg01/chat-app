2eb331cb0781dd328cff3fceceb220ad9ff03ff4
amitg01 <amitg.ghy@gmail.com>
2023-11-03 20:19:56 +0530

    final setup

--- server/public/app.js
+++ server/public/app.js
@@ -1,20 +1,19 @@
-const socket = io("ws://localhost:3500");
+const socket = io("https://chat-app-ygkm.onrender.com");
 
 const msgInput = document.querySelector("#message");
 const nameInput = document.querySelector("#name");
-const roomInput = document.querySelector("room");
+const chatRoom = document.querySelector("#room");
 const activity = document.querySelector(".activity");
-const userList = document.querySelector(".user-list");
+const usersList = document.querySelector(".user-list");
 const roomList = document.querySelector(".room-list");
 const chatDisplay = document.querySelector(".chat-display");
 
 function sendMessage(e) {
   e.preventDefault();
-  if (msgInput.value || nameInput.value || roomInput.value) {
+  if (nameInput.value && msgInput.value && chatRoom.value) {
     socket.emit("message", {
-      text: msgInput.value,
       name: nameInput.value,
-      room: roomInput.value,
+      text: msgInput.value,
     });
     msgInput.value = "";
   }
@@ -38,19 +37,73 @@ document.querySelector(".form-join").addEventListener("submit", enterRoom);
 msgInput.addEventListener("keypress", () => {
   socket.emit("activity", nameInput.value);
 });
+
 // Listen for messages
-
 socket.on("message", (data) => {
+  activity.textContent = "";
+  const { name, text, time } = data;
   const li = document.createElement("li");
-  li.textContent = data;
-  document.querySelector("ul").appendChild(li);
+  li.className = "post";
+  if (name === nameInput.value) li.className = "post post--right";
+  if (name !== nameInput.value && name !== "Admin")
+    li.className = "post post--left";
+  if (name !== "Admin") {
+    li.innerHTML = `<div class="post__header ${
+      name === nameInput.value ? "post__header--user" : "post__header--reply"
+    }">
+        <span class="post__header--name">${name}</span> 
+        <span class="post__header--time">${time}</span> 
+        </div>
+        <div class="post__text">${text}</div>`;
+  } else {
+    li.innerHTML = `<div class="post__text">${text}</div>`;
+  }
+  document.querySelector(".chat-display").appendChild(li);
+
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
-let t;
+let activityTimer;
 socket.on("activity", (name) => {
-  clearTimeout(t);
-  t = setTimeout(() => {
-    document.querySelector(".activity").textContent = "";
-  }, 1000);
-  document.querySelector(".activity").textContent = `${name} is typing...`;
+  activity.textContent = `${name} is typing...`;
+
+  // Clear after 3 seconds
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 3000);
 });
+
+socket.on("userList", ({ users }) => {
+  showUsers(users);
+});
+
+socket.on("roomList", ({ rooms }) => {
+  showRooms(rooms);
+});
+
+function showUsers(users) {
+  usersList.textContent = "";
+  if (users) {
+    usersList.innerHTML = `<em>Users in ${chatRoom.value}:</em>`;
+    users.forEach((user, i) => {
+      usersList.textContent += ` ${user.name}`;
+      if (users.length > 1 && i !== users.length - 1) {
+        usersList.textContent += ",";
+      }
+    });
+  }
+}
+
+function showRooms(rooms) {
+  roomList.textContent = "";
+  if (rooms) {
+    roomList.innerHTML = "<em>Active Rooms:</em>";
+    rooms.forEach((room, i) => {
+      roomList.textContent += ` ${room}`;
+      if (rooms.length > 1 && i !== rooms.length - 1) {
+        roomList.textContent += ",";
+      }
+    });
+  }
+}
